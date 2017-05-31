352fabce934f42b51227f75fb9162434e5209960
ali-augmedix <alihossain@augmedix.com>
2017-05-31 17:09:24 +0600

    Initial commit

---
+++ app/src/main/java/com/android/androidarchcomponents/listItems/MainActivity.java
@@ -0,0 +1,72 @@
+package com.android.androidarchcomponents.listItems;
+
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.android.androidarchcomponents.R;
+import com.android.androidarchcomponents.addItem.AddActivity;
+import com.android.androidarchcomponents.db.AppDatabase;
+import com.android.androidarchcomponents.db.entity.BorrowModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends LifecycleActivity implements View.OnLongClickListener {
+
+    private BorrowedListViewModel viewModel;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerViewAdapter = new RecyclerViewAdapter(new ArrayList<BorrowModel>(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        viewModel = ViewModelProviders.of(this).get(BorrowedListViewModel.class);
+
+        viewModel.getItemAndPersonList().observe(MainActivity.this, new Observer<List<BorrowModel>>() {
+            @Override
+            public void onChanged(@Nullable List<BorrowModel> itemAndPeople) {
+                recyclerViewAdapter.addItems(itemAndPeople);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        AppDatabase.destroyInstance();
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        BorrowModel borrowModel = (BorrowModel) v.getTag();
+        viewModel.deleteItem(borrowModel);
+        return true;
+    }
+}
